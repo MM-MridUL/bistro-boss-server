2e09a54483cc401e8f4a088436f18a85cc2a21c4
MM-MridUL <mmmridul84@gmail.com>
2025-03-18 03:43:58 -0700

    Create jwt token

--- index.js
+++ index.js
@@ -32,8 +32,22 @@ async function run() {
     const reviewsCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
+    // jwt related api
+    app.post('/jwt',async(req,res)=>{
+      const user =req.body;
+      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'});
+      res.send({token})
+    })
+
+    // middlewares
+    const verifyToken = (req,res,next)=>{
+      console.log('inside verify token',req.headers);
+      next();
+    }
+
     // users related api
-    app.get('/users',async(req,res)=>{
+    app.get('/users',verifyToken,async(req,res)=>{
+      
       const result = await userCollection.find().toArray();
       res.send(result);
     });

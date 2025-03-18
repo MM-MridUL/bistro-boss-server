eaa2427b840b5f4cb3521a296afd6cc19ff3aea9
MM-MridUL <mmmridul84@gmail.com>
2025-03-18 05:01:02 -0700

    added get api for check admin or user

--- index.js
+++ index.js
@@ -41,8 +41,18 @@ async function run() {
 
     // middlewares
     const verifyToken = (req,res,next)=>{
-      console.log('inside verify token',req.headers);
-      next();
+      console.log('inside verify token',req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message:'forbidden access'});
+      }
+      const token=req.headers.authorization.split(' ')[1];
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+          return res.status(401).send({message:'forbiden access'})
+        }
+        req.decoded =decoded;
+        next();
+      })
     }
 
     // users related api
@@ -51,6 +61,20 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     });
+
+    app.get('/users/admin/:email',verifyToken,async(req,res)=>{
+      const email = req.params.email;
+      if(email !==req.decoded.email){
+        return res.status(403).send({message: 'unauthorized access'})
+      }
+      const query = {email:email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+      }
+      res.send({admin});
+    })
 
     app.post('/users',async(req,res)=>{
       const user = req.body;

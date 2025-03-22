2b5d3d09064ab201b218fecfd312d6ffd475076a
MM-MridUL <mmmridul84@gmail.com>
2025-03-22 03:08:44 -0700

    Created payment get API using email query

--- index.js
+++ index.js
@@ -207,6 +207,16 @@ async function run() {
         clientSecret: paymentIntent.client_secret
       })
     })
+    
+    app.get('/payment/:email',verifyToken,async(req,res)=>{
+      const query = {email:req.params.email};
+      if(req.params.email!==req.decoded.email){
+        return res.status(403).send({message: 'forbidden-access'})
+
+      }
+      const result = await paymentCollection.find(query).toArray();
+      res.send(result);
+    })
 
     app.post('/payments',async(req,res)=>{
       const payment = req.body;

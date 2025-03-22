0878b35d9b400fe9850905ef32fde02f8049810a
MM-MridUL <mmmridul84@gmail.com>
2025-03-22 02:32:43 -0700

    Save payment info and delete cart items from the databse

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ async function run() {
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewsCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
+    const paymentCollection = client.db("bistroDb").collection("payments");
 
     // jwt related api
     app.post('/jwt',async(req,res)=>{
@@ -195,7 +196,31 @@ async function run() {
     app.post('/create-payment-intent',async(req,res)=>{
       const {price}=req.body;
       const amount = parseInt(price*100);
+      console.log('amount',amount);
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency:"usd",
+        payment_method_types:['card']
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     })
+
+    app.post('/payments',async(req,res)=>{
+      const payment = req.body;
+      const paymentResult = await paymentCollection.insertOne(payment);
+
+      // carefully delete each item from the cart
+      console.log('payment info',payment)
+      const query ={_id: {
+        $in: payment.cartIds.map(id=>new ObjectId(id))
+      }};
+      const deleteResult = await cartCollection.deleteMany(query);
+      res.send({paymentResult,deleteResult});
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
